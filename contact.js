1c5c3c3ca83dec2b64e46ad11544f757b8502855
Lucía Puga <lucia.puga@enti.cat>
2025-01-17 11:18:51 +0100

    Form updates

---
+++ contact.js
@@ -0,0 +1,33 @@
+function procesa_envio(event)
+{
+	event.preventDefault();
+	
+	console.log("Procesando envío.");
+	
+	let nombre = document.getElementById("name");
+	let salida = document.getElementById("salida");
+	
+	if (nombre.value.length < 2){
+		salida.value = "El nombre debe tener al menos 2 caracteres.";
+		
+		salida.style.color = "#ff0000";
+		nombre.style.color = "#ff0000";
+		nombre.style.border = "2px solid red";
+		
+		return false;
+	}
+	
+	let email = document.getElementById("email");
+	
+	if (email.value.length < 6){
+		salida.value = "El email debe tener al menos 6 caracteres.";
+		
+		salida.style.color = "#ff0000";
+		email.style.color = "#ff0000";
+		email.style.border = "2px solid red";
+		
+		return false;
+	}
+	
+	document.getElementById("form_contacto").submit();
+}
